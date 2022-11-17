5e353c90496d97f19ec71cdbf93d67cecfcad6f7
Raul Val <raulval@outlook.com>
2022-11-17 00:22:06 -0300

    🐛 change backend port

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 // const cookieParser = require("cookie-parser");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3333;
 
 const app = express();
 
